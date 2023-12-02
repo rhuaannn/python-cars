f061a862dcc90f45e9248913f18f373bdab61c33
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-02 17:08:39 -0300

    feat: created user

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
-
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars', cars_view, name='cars_list'),
-    path('new_car', new_car_view, name='new_car')
+    path('new_car', new_car_view, name='new_car'),
+    path('register', register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -23,7 +23,7 @@ def new_car_view(request):
     
     if request.method == 'POST':
         new_car_form = CarModelForm(request.POST, request.FILES)
-        print(new_car_form.data)
+       
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
