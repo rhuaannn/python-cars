1f2e7711e297e03e0afaab3e415f7f47379ea491
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-03 01:15:05 -0300

    feat: created login, logout, list.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
  
@@ -8,7 +9,7 @@ def register_view(request):
         if user_form.is_valid():
             user_form.save()
             
-            return redirect('cars_list')
+            return redirect('login')
 
     else:                
         user_form = UserCreationForm()
@@ -16,5 +17,21 @@ def register_view(request):
 
 
 def login_view(request):
-    login_form = AuthenticationForm()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']    
+        user = authenticate(request, username=username, password= password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    
+    return redirect('cars_list')
