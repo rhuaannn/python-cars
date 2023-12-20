87ae6d35830c00bb0b1578f044858d309191316a
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-20 12:10:20 -0300

    finish project

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarCreateView, CarDetailView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView,CarDeleteView
 from accounts.views import register_view, login_view, logout_view
  
 
@@ -14,4 +14,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
+    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
+from django.urls import reverse_lazy
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
  
 
 class CarsListView(ListView):
@@ -29,3 +30,17 @@ class CarDetailView(DetailView):
     
     model = Car
     template_name ='car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model  =  Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+   
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+    
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
