96b3bb5992157f04900892a808cbaac2532a4685
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-25 10:59:30 -0300

    feat: add singnals para car_invetory. Trazendo o count de cars e value de todos os cars

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car, Brand
+from cars.models import Car, Brand, CarInventory
  
 class CarAdmin(admin.ModelAdmin):
     # list display é a colunas da tabela car que vai ser criada no painel admim.
@@ -11,6 +11,12 @@ class CarAdmin(admin.ModelAdmin):
 class BrandAdmin(admin.ModelAdmin):
    list_display = ('name',)
    search_fields = ('name',)
+   
+class CarInventoryAdmin(admin.ModelAdmin):
+    list_display= ('cars_count', 'cars_value', 'created_at')
+    search_fields = ('model', 'cars__count')    
+    
     
 admin.site.register(Car,CarAdmin)
 admin.site.register(Brand,BrandAdmin)
+admin.site.register(CarInventory, CarInventoryAdmin)
--- cars/sginals.py
+++ cars/sginals.py
@@ -1,24 +1,21 @@
-from django.db.models.signals import pre_save, pre_delete, pre_init,post_delete,post_save
+from django.db.models.signals import pre_save, pre_delete, post_delete,post_save
 from django.dispatch import receiver
+from django.db.models import Sum
 
 from cars.models import Car
-
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('## PRE ##')
     
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('## POST ##')
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value = Sum('value')
+    )
     
-    
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('## PRE ##')
-    
+ 
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
     print('## POST ##')
     print('##', instance)
+    
