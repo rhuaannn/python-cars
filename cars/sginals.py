0045e630cbe81a5baada8a75b65f3bc9bf5e1b09
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-25 11:04:18 -0300

    feat: create o resultado da query no banco

--- cars/sginals.py
+++ cars/sginals.py
@@ -2,8 +2,7 @@ from django.db.models.signals import pre_save, pre_delete, post_delete,post_save
 from django.dispatch import receiver
 from django.db.models import Sum
 
-from cars.models import Car
-    
+from cars.models import Car, CarInventory    
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
@@ -11,8 +10,11 @@ def car_post_save(sender, instance, **kwargs):
     cars_value = Car.objects.aggregate(
         total_value = Sum('value')
     )['total_value']
-    
- 
+    CarInventory.objects.create(
+        cars_count = cars_count,
+        cars_value = cars_value
+    )      
+        
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
