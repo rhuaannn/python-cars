5cbb1c69fb1a4fd7f6ca7c9844d7cab14a17a118
rhuaannn <rhuanmarques1223@gmail.com>
2023-12-26 15:47:10 -0300

    fix: remove api key

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 import openai
 
 def get_car_ai_bio(model, brand, year):
@@ -5,12 +6,14 @@ def get_car_ai_bio(model, brand, year):
     Me mostra uma descrção de venda para o carro {} {} {} em apenas 200 caracteres. Fale coisa especifica 
     do carro.
     '''
-    openai.api_key = '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'
+    openai.api_key = ''
+    client = OpenAI(
+        organization = '',
+    )
     prompt = prompt.format(brand, model, year)
-    response = openai.completions.create(
-        model = 'text-davinci-003',   
-        prompt = '',
+    response = client.create(
+        model='text-davinci-003',
+        prompt=prompt,
         max_tokens=1000,
-        
     )
     return response['choices'][0]['text']
